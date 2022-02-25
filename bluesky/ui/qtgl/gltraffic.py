ea7259554fd8660ba16c21c0abd5ebf1835f1af8
ilabs-kdc <93335573+ilabs-kdc@users.noreply.github.com>
2022-02-25 16:06:12 +0100

    T-Bar DTG Label Plugin

--- bluesky/ui/qtgl/gltraffic.py
+++ bluesky/ui/qtgl/gltraffic.py
@@ -69,7 +69,6 @@ class Traffic(glh.RenderObject, layer=100):
         self.lbl_lvnl   = glh.GLBuffer()
         self.ssrlbl     = glh.GLBuffer()
         self.mlbl       = glh.GLBuffer()
-        self.pluginlbl  = None
 
         self.lbloffset  = glh.GLBuffer()
 
@@ -96,10 +95,11 @@ class Traffic(glh.RenderObject, layer=100):
         self.leaderlines    = glh.Line()
 
         # --------------- Plugin Variables ---------------
-        self.pluginlabelpos  = None
-        self.pluginlbloffset = None
-        self.pluginlbl       = None
-        self.pluginlabel     = None
+        self.show_pluginlabel = False
+        self.pluginlabelpos   = None
+        self.pluginlbloffset  = None
+        self.pluginlbl        = None
+        self.pluginlabel      = None
 
         bs.net.actnodedata_changed.connect(self.actdata_changed)
 
@@ -271,8 +271,7 @@ class Traffic(glh.RenderObject, layer=100):
             self.aclabels_lvnl.draw(n_instances=actdata.naircraft)
             self.ssrlabels.draw(n_instances=actdata.naircraft)
             self.microlabels.draw(n_instances=actdata.naircraft)
-            if self.pluginlabel is not None:
-                print('draw plugin')
+            if self.pluginlabel is not None and self.show_pluginlabel:
                 self.pluginlabel.draw(n_instances=actdata.naircraft)
 
             self.leaderlines.draw()
@@ -531,6 +530,8 @@ class Traffic(glh.RenderObject, layer=100):
                 self.labelpos = labelpos
                 self.id_prev = data.id
                 self.lbloffset.update(np.array(self.labelpos, dtype=np.float32))
+                if self.pluginlbloffset is not None:
+                    self.pluginlbloffset.update(np.array(self.labelpos+self.pluginlabelpos, dtype=np.float32))
                 # Leader line update
                 self.leaderlinepos = leaderlinepos
                 self.leaderlines.update(vertex=self.leaderlinepos, lat=data.lat, lon=data.lon, color=color)
@@ -579,6 +580,8 @@ class Traffic(glh.RenderObject, layer=100):
 
             # Update label offset
             self.lbloffset.update(np.array(self.labelpos, dtype=np.float32))
+            if self.pluginlbloffset is not None:
+                self.pluginlbloffset.update(np.array(self.labelpos+self.pluginlabelpos, dtype=np.float32))
 
             # Leader lines
             self.leaderlinepos[idx] = leaderline_vertices(actdata, self.labelpos[idx][0], self.labelpos[idx][1])
@@ -590,18 +593,25 @@ class Traffic(glh.RenderObject, layer=100):
         Function: Initialize and create plugin buffers and attributes
         Args:
             blocksize:  Label block size [tuple]
-            position:   Label offset [tuple]
+            position:   Text position (line (y), character (x))  [tuple]
         Returns: -
 
         Created by: Bob van Dillen
         Date: 25-2-2022
         """
 
-        # Node data
-        actdata = bs.net.get_nodedata()
+        self.glsurface.makeCurrent()
+
+        # Sizes
+        ac_size = settings.ac_size
+        text_size = settings.text_size
+        text_width = text_size
+        text_height = text_size * 1.2307692307692308
+
+        # Process position
+        self.pluginlabelpos = np.array([position[1]*text_width, -position[0]*text_height], dtype=np.float32)
 
         # Initialize
-        self.pluginlabelpos  = np.ones((actdata.naircraft, 2), dtype=np.float32)*offset
         self.pluginlbl       = glh.GLBuffer()
         self.pluginlbloffset = glh.GLBuffer()
         self.pluginlabel     = glh.Text(settings.text_size, blocksize)
@@ -610,6 +620,13 @@ class Traffic(glh.RenderObject, layer=100):
         self.pluginlbl.create(MAX_NAIRCRAFT * 24, glh.GLBuffer.StreamDraw)
         self.pluginlbloffset.create(MAX_NAIRCRAFT * 24, glh.GLBuffer.StreamDraw)
         self.pluginlabel.create(self.pluginlbl, self.lat, self.lon, self.color, self.pluginlbloffset, instanced=True)
+
+        # Update position
+        if len(self.labelpos) != 0:
+            self.pluginlbloffset.update(np.array(self.labelpos+self.pluginlabelpos, dtype=np.float32))
+
+        # Draw
+        self.show_pluginlabel = True
 
 
 """
--- plugins/T-Bar.py
+++ plugins/T-Bar.py
@@ -2,14 +2,16 @@
 
 import numpy as np
 import bluesky as bs
-from bluesky.ui.qtgl.gltraffic import Traffic
 from bluesky import core, stack
+from bluesky.ui.qtgl import console
+from bluesky.ui.qtgl.gltraffic import Traffic, leading_zeros
 
 
+### Initialization function of the plugin
 def init_plugin():
     ''' Plugin initialisation function. '''
 
-    commands = Commands()
+    dtg = TBar()
 
     # Configuration parameters
     config = {
@@ -24,32 +26,83 @@ def init_plugin():
     return config
 
 
-class Commands(core.Entity):
+'''
+Classes
+'''
+
+
+class TBar(core.Entity):
+    """
+    Definition: Class used to initialize and update DTG to T-Bar points
+    Methods:
+        showdtg():      Initialize the DTG label
+        update_tbar():  Update T-Bar graphics
+    """
     def __init__(self):
         super().__init__()
         self.initialized = False
         self.tbarlbl = None
-        self.lbloffset = []
+
+        # Set update function
+        bs.net.actnodedata_changed.connect(self.update_tbar)
 
     @stack.command(name='SHOWDTG',)
     def showdtg(self):
+        """
+        Function: Initialize the DTG label
+        Args: -
+        Returns: -
+
+        Created by: Bob van Dillen
+        Date: 25-2-2022
+        """
+
+        # Check if we need to initialize
         if not self.initialized:
-            self.tbarlbl = TBarLabel()
-            self.tbarlbl.plugin_init((4, 1))
-            #actdata = bs.net.no
-            bs.net.actnodedata_changed.connect(self.updatedtg)
-            print('init finished')
+            # Get current node data
+            actdata = bs.net.get_nodedata()
 
-    def updatedtg(self, nodeid, nodedata, changed_elems):
-        if 'ACDATA' in changed_elems:
+            # Class to access Traffic graphics
+            self.tbarlbl = Traffic()
+
+            # Initialize plugin label
+            self.tbarlbl.plugin_init(blocksize=(3, 1), position=(2, 7))
+
+            # Update label with current data
             rawlabel = ''
-            for idx in range(len(nodedata.acdata.id)):
-                rawlabel += '1234'
+            for idx in range(len(actdata.acdata.id)):
+                rawlabel += 3*' '
+
             self.tbarlbl.pluginlbl.update(np.array(rawlabel.encode('utf8'), dtype=np.string_))
-            print('update')
 
+            # Initialization completed
+            self.initialized = True
+        else:
+            self.tbarlbl.show_pluginlabel = not self.tbarlbl.show_pluginlabel
 
-class TBarLabel(Traffic):
-    def __init__(self):
-        super(TBarLabel, self).__init__()
+    def update_tbar(self, nodeid, nodedata, changed_elems):
+        """
+        Function: Update T-Bar graphics
+        Args:
+            nodeid:         Node identifier []
+            nodedata:       Node data [class]
+            changed_elems:  Changed elements [list]
+        Returns: -
 
+        Created by: Bob van Dillen
+        Date: 25-2-2022
+        """
+
+        if self.initialized:
+            if 'ACDATA' in changed_elems:
+                # Update DTG label
+                rawlabel = ''
+
+                for idx in range(len(nodedata.acdata.id)):
+                    acid = nodedata.acdata.id[idx]
+                    dtg = nodedata.acdata.dtg[idx]
+                    if acid == console.Console._instance.id_select and dtg != 0.:
+                        rawlabel += '%-3s' % leading_zeros(dtg)[:3]
+                    else:
+                        rawlabel += 3*' '
+                self.tbarlbl.pluginlbl.update(np.array(rawlabel.encode('utf8'), dtype=np.string_))
